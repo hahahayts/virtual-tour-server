3d25af01898683c0b5cb15d0fc7912ac357d4e70
mrf3v3r <jojehterce@gmail.com>
2025-11-26 19:59:57 +0800

    fix: update import path for PrismaClient in prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma"; // relative path to the generated client
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
